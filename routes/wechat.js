c5850de39cfe78587814de4dc7cc2b223b64b904
jinshiyi588 <jinshiyi588@163.com>
2016-08-24 17:21:04 +0800

    install wechat

---
+++ routes/wechat.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', wechat('jsy_token', function (req, res, next) {  
+    // 微信输入信息都在req.weixin上  
+    var message = req.weixin;  
+    console.log(message);  
+  
+    if((message.MsgType == 'event') && (message.Event == 'subscribe'))  
+    {  
+                     
+        var replyStr = "感谢你的关注！";  
+        res.reply(replyStr);  
+    }  
+}));  
+
+
+module.exports = router;
